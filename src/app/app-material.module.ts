39363f71a0178604ceb9ef8bf77519cb3c55955e
TNougaillac <tom.nougaillac@hotmail.fr>
2023-01-22 15:40:01 +0100

    update

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
-import {MatSlideToggleChange, MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatInputModule} from "@angular/material/input";
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   exports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppMaterialModule } from './app-material.module';
+import {AppComponent} from "./app.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {NavbarComponent} from "./ldap-management/navbar/navbar.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { LdapManagementModule } from './ldap-management/ldap-management.module';
+import {AppMaterialModule} from "./app-material.module";
+import {LdapManagementModule} from "./ldap-management/ldap-management.module";
 import {AppRoutingModule} from "./app-routing.module";
-import {NavbarComponent} from "./ldap-management/navbar/navbar.component";
 
 @NgModule({
   declarations: [
